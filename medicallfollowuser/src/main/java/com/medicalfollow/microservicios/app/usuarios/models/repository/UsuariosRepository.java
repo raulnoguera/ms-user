fc5a23c237ec9681c3038c047dc0cc07908a8722
raulnoguera <33468997+raulnoguera@users.noreply.github.com>
2022-07-22 15:31:59 -0400

    Delete medicallfollowuser directory

--- medicallfollowuser/src/main/java/com/medicalfollow/microservicios/app/usuarios/models/repository/UsuariosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicalfollow.microservicios.app.usuarios.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.medicalfollow.microservicios.app.usuarios.models.entity.Usuarios;
-
-public interface UsuariosRepository extends CrudRepository<Usuarios, Long> {
-
-}
